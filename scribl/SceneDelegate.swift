5ea630e6b9911f08b3f76598c55baf8f4cf45d38
Adam Bollard <adambollard@gmail.com>
2020-08-19 19:39:20 -0400

    rip out core data in favor of firestore

--- scribl/SceneDelegate.swift
+++ scribl/SceneDelegate.swift
@@ -42,7 +42,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        
     }
     
     private func configureNavBarAppearance(navController: UINavigationController) {
